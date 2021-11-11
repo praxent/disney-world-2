2f18b3aa27a27843139867c088fac28b64b3732c
Rene Lopez <r.alex.lm@outlook.com>
2021-11-11 13:02:46 -0600

    kinda working

--- src/magic-kingdom/activity2/Activity2.tsx
+++ src/magic-kingdom/activity2/Activity2.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from "styled-components";
+import libertyTree from './liberty-tree-tavern.jpg';
 
 const Styles = styled.div`
   color: blue;
@@ -11,10 +12,49 @@ const Styles = styled.div`
 `;
 
 function Activity2() {
+  var [items, setItems] = useState([]);
+  useEffect(() => {
+  const url = "http://localhost:4000/liberty_tree";
+
+  const fetchData = async () => {
+      try {
+          const response = await fetch(url);
+          const json = await response.json();
+          setItems(json);
+          console.dir(json);
+      } catch (error) {
+          console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  function buyItem(item : object, index : string) {
+    let newItems = items;
+    newItems[parseInt(index)].quantity--;
+    setItems(newItems);
+    // setItems({
+    //   items.map(el => (el.id === index ? Object.assign({}, el, { item }) : el))
+    // });
+    console.log("bought " + item.name + ". Remaining: " + item.quantity);
+  }
+
   return (
     <Styles>
       <div className="container">
         Hello from Activity 2 Magic Kingdom
+        <br />
+        <img src={libertyTree}/>;
+        <br />
+        <div className="items-grid">
+          {items.map((item,index)=>{
+              return <div className="item-row">
+                <div className="item-col">{item.name}</div>
+                <div className="item-col">{item.quantity}</div>
+                <div className="item-col"><button onClick={() => buyItem(item, index)}>Buy one!</button></div>
+              </div>
+          })}
+        </div>
       </div>
     </Styles>
   )
