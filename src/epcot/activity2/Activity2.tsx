f77a82679041a0eba7be394ba2a2e5c6fb4446d0
Roberto <rgarcia@healthcarefinancedirect.com>
2021-11-11 13:04:29 -0600

    activity 2

--- src/epcot/activity2/Activity2.tsx
+++ src/epcot/activity2/Activity2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 
 const Styles = styled.div`
@@ -8,16 +8,45 @@ const Styles = styled.div`
     max-width: 900px;
     margin: 0 auto;
   }
+
+img{
+width: 600px;}
 `;
 
 function Activity2() {
-  return (
-    <Styles>
-      <div className="container">
-        Hello from Activity 2 Epcot
-      </div>
-    </Styles>
-  )
+    const [count, setCount] = useState(0);
+    const [menu, setMenu] = useState(Array);
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+    return (
+        <Styles>
+            <div className="container">
+                <img src='http://localhost:3000/captain-only/images/space-220.jpg' />
+            </div>
+            <div>
+                <p>You clicked {count} times</p>
+                <button onClick={() => {
+                    setCount(count + 1);
+                    fetch('http://localhost:4000/space_220')
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log(data);
+                            setMenu(data);
+                        });
+                }}>
+                    Click me
+                </button>
+                <ul>
+                    {menu.map(name => (
+                        <li>
+                            item: {name.name} description: {name.description} add to cart
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </Styles >
+    )
 }
 
 export default Activity2
