19a5c6e68a3307fe21a2f4d621b0d0c8d3d99942
Jesus6Connex <jebus.velazquez@gmail.com>
2021-11-12 11:20:16 -0600

    add dino prize and dino track

--- src/animal-kingdom/activity1/Activity1.tsx
+++ src/animal-kingdom/activity1/Activity1.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 
 import splash from "./images/splash.png";
@@ -76,29 +76,76 @@ const Styles = styled.div`
   .water-gun-trigger:active {
     transform: scale(0.9);
   }
+
+  .slider-container {
+    width: 100%;
+    margin-top: 20px;
+    margin-bottom: 40px;
+  }
+  .slider {
+    width: 100%;
+    height: 80px;
+    border-radius: 5px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    position: relative;
+  }
+  .slider-image {
+    height: 100px;
+    border: 0;
+    position: absolute;
+  }
 `;
 
 function Activity1() {
   const [shooting, setShooting] = useState(false);
+  const [sliderPosition, setSliderPosition] = useState(0); // 0-800
+
   return (
     <Styles>
       <div className="container">
         <h1>Fossil Fun Games</h1>
-        <h2>Shoot!</h2>
+        <div className="slider-container">
+          <div className="slider">
+            <img
+              src="https://www.pikpng.com/pngl/b/381-3817218_jurassic-world-dinosaur-png-png-download-jurassic-park.png"
+              alt="dino" 
+              className="slider-image"
+              style={{ left: sliderPosition }} 
+            />
+          </div>
+        </div>
         <div className="game">
           <div className="target-container">
             <img className="target" src={target} alt="target" />
             {shooting && <img className="splash" src={splash} alt="splash" />}
           </div>
-          <SquirtGun setShooting={setShooting} shooting={shooting}/>
+          <SquirtGun
+            setShooting={setShooting}
+            setSliderPosition={setSliderPosition}
+            shooting={shooting}
+            position={sliderPosition}
+          />
         </div>
+        {sliderPosition >= 800 && 
+          <div>
+            <h2>You Win! Have a dino</h2>
+            <img src="https://target.scene7.com/is/image/Target/GUEST_60454535-c70a-4d3c-abe8-a2adb402699a?wid=488&hei=488&fmt=pjpeg" alt="" />
+          </div>
+        }
       </div>
     </Styles>
   )
 }
 
 function SquirtGun(props) {
-  const onClick = () => props.setShooting(!props.shooting);
+  const onClick = async () => {
+    props.setShooting(!props.shooting);
+    if (props.position < 800 && !props.shooting) {
+      props.setSliderPosition(props.position + 100);
+    }
+  }
   return (
     <button onClick={onClick} className="water-gun-trigger">
       <img className="waterGun" src={waterGun} alt="water gun" />
