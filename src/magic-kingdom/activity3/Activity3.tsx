97d3f538f355d2f9ed6864469fe1a585f7b07a2e
Rene Lopez <r.alex.lm@outlook.com>
2021-11-11 22:25:23 -0600

    pretty sure is not the correct approach

--- src/magic-kingdom/activity3/Activity3.tsx
+++ src/magic-kingdom/activity3/Activity3.tsx
@@ -1,5 +1,10 @@
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 import React from 'react';
 import styled from "styled-components";
+import light from './light-element.jpg';
+import dark from './dark-element.jpg';
+import fire from './fire-element.jpg';
+import water from './water-element.jpg';
 
 const Styles = styled.div`
   color: green;
@@ -8,16 +13,45 @@ const Styles = styled.div`
     max-width: 900px;
     margin: 0 auto;
   }
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
 `;
 
+function DarkSpell() {
+  return (<img src={dark} className="width=50px;height=50px" />)
+}
+function LightSpell() {
+  return (<img src={light} className="width=50px;height=50px" />)
+}
+function FireSpell() {
+  return (<img src={fire} className="width=50px;height=50px" />)
+}
+function WaterSpell() {
+  return (<img src={water} className="width=50px;height=50px" />)
+}
+
 function Activity3() {
+  
   return (
     <Styles>
       <div className="container">
         Hello from Activity 3 Magic Kingdom
-      </div>
+        <br />
+        <Link to="light">Light Spell</Link>
+        <br />
+        <Link to="dark">Dark Spell</Link>
+        <br />
+        <Link to="fire">Fire Spell</Link>
+        <br />
+        <Link to="water">Water Spell</Link>
+        <br />
+        <Outlet />
+      </div>  
     </Styles>
   )
 }
 
-export default Activity3
+export default { Activity3, DarkSpell, LightSpell, FireSpell, WaterSpell };
